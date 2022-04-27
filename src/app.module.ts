51e6e6ff0805efc9d757e7c45064a1d7b5a0756d
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2022-04-27 17:50:38 -0300

    qua 27 abr 2022 17:50:38 -03

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,7 +7,21 @@ import { ExpensesModule } from './expenses/expenses.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule, ExpensesModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', 
+      port: 5432,
+      host: 'ec2-52-21-136-176.compute-1.amazonaws.com',
+      username: 'qlzxbrwvqultnp',
+      database: 'd7rl1llkd4np2n',
+      password: 'a54424e06e18c0920797b42c891b3fcf3f203487d839ff4cc2f0917c854d8726',
+      entities: [__dirname+'/../**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+    TasksModule, 
+    ExpensesModule, 
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
